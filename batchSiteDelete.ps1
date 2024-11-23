32d92caff97eac405124feda4e6c672cf96d460f
junyan li <jiangzhonping@outlook.com>
2024-11-23 23:36:51 +0800

    增加自动处理时候的提示信息

--- batchSiteDelete.ps1
+++ batchSiteDelete.ps1
@@ -11,6 +11,15 @@ function remove-sites {
         Remove-SPOSite -Identity  $url -Confirm:$false
     }
 }
+function waitPolicyDistribution {
+    $policy = Get-RetentionCompliancePolicy -Identity "Lenovo retention" -DistributionDetail
+    while ($policy.DistributionStatus -eq "Pending") {
+        Write-Host "Distribution status is $($policy.DistributionStatus)" -ForegroundColor Yellow
+        Start-Sleep -Seconds 60
+        $policy = Get-RetentionCompliancePolicy -Identity "Lenovo retention" -DistributionDetail
+    }
+    Write-Host "Distribution status is Success" -ForegroundColor Green
+}
 
 $sites = Import-Csv -Path $csv_path
 $batchSites_str = ""
