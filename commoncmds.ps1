78aa6326214635b7a1773dcd77208a7254725884
junyan li <jiangzhonping@outlook.com>
2024-11-26 20:54:27 +0800

    增加已删除站点导出和查看数量变化

--- commoncmds.ps1
+++ commoncmds.ps1
@@ -1,4 +1,6 @@
  $dateStr=Get-Date -Format "yyyyMMdd"
-Get-SPODeletedSite -IncludePersonalSite -Limit ALL | export-csv -Path "D:\sftp\deletedSPOsite_$dateStr.csv" -NoTypeInformation
+Get-SPODeletedSite -IncludePersonalSite -Limit ALL | 
+Select-Object Url, Status, ArchiveStatus, DeletionTime, DaysRemaining | 
+export-csv -Path "D:\sftp\deletedSPOsite_$dateStr.csv" -NoTypeInformation
 
  Get-RetentionCompliancePolicy -Identity "Lenovo retention" -DistributionDetail | Format-List
